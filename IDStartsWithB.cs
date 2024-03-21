f29500a28a4c0b1ba4a0d7a6961d600a6c8585bc
Sam Gilchrist <samofsand@gmail.com>
2024-03-21 14:53:56 -0500

    Create IDStartsWithB.cs
    IDStartsWithB
    -Prints out IDs from provided string list that start with "B"

---
+++ IDStartsWithB.cs
@@ -0,0 +1,8 @@
+﻿string[] fraudulentOrderID = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+foreach (string IDs in fraudulentOrderID)
+{
+    if (IDs.StartsWith("B"))
+    {
+        Console.WriteLine($"{IDs}");
+    }
+}
